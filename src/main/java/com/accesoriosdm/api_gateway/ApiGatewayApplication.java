2149e26251e7c52ba1696b542e978a97ac93f0e5
sebastiian06 <jsagudeloq@corhuila.edu.co>
2026-03-03 16:17:20 -0500

    feat: agregar api gateway

---
+++ src/main/java/com/accesoriosdm/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.accesoriosdm.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
